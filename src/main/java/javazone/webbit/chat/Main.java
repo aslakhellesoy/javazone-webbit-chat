020dc33cecb3326760af553b47fbf3717008f0be
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-09-05 16:17:22 +0100

    Serve statis files from Webbit

--- src/main/java/javazone/webbit/chat/Main.java
+++ src/main/java/javazone/webbit/chat/Main.java
@@ -1,8 +1,15 @@
 package javazone.webbit.chat;
 
+import org.webbitserver.WebServer;
+import org.webbitserver.handler.StaticFileHandler;
+import org.webbitserver.netty.NettyWebServer;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        WebServer server = new NettyWebServer(8080);
+        server.add(new StaticFileHandler("src/main/resources/web"));
+        server.start();
     }
 }
