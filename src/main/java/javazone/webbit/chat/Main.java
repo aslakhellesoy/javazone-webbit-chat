530db90d3bc8b31927810c33fc65c63e9f3903eb
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-09-05 16:33:33 +0100

    Connect a websocket with client side logging. Add a logging handler in Webbit.

--- src/main/java/javazone/webbit/chat/Main.java
+++ src/main/java/javazone/webbit/chat/Main.java
@@ -2,6 +2,8 @@ package javazone.webbit.chat;
 
 import org.webbitserver.WebServer;
 import org.webbitserver.handler.StaticFileHandler;
+import org.webbitserver.handler.logging.LoggingHandler;
+import org.webbitserver.handler.logging.SimpleLogSink;
 import org.webbitserver.netty.NettyWebServer;
 
 import java.io.IOException;
@@ -9,6 +11,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         WebServer server = new NettyWebServer(8080);
+        server.add(new LoggingHandler(new SimpleLogSink()));
         server.add(new StaticFileHandler("src/main/resources/web"));
         server.start();
     }
--- src/main/resources/web/chatroom.js
+++ src/main/resources/web/chatroom.js
@@ -1,6 +1,12 @@
 window.onload = function() {
+    var ws = new WebSocket('ws://' + document.location.host + "/chat");
+    ws.onopen    = function(e) { console.log('OPEN'); };
+    ws.onclose   = function(e) { console.log('CLOSE'); };
+    ws.onerror   = function(e) { console.log('ERROR'); };
+    ws.onmessage = function(e) { console.log('MESSAGE'); };
+
     function send(message) {
-        console.log(message);
+        ws.send(message);
     }
 
     var entry = document.getElementById('entry');
